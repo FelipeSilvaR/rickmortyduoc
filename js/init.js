e34d5675b6c9a0b891cbe5b2d4cbea586434b64b
felipe silva <felipe.silvar85@gmail.com>
2020-11-20 22:52:06 -0300

    Agregar base de app rick y morty Agregar la estructura base de la app Integrar axios Consumir endpoint de personajes

---
+++ js/init.js
@@ -0,0 +1,6 @@
+//Escuchando cuando se carga la página
+window.addEventListener('DOMContentLoaded', async ()=>{
+    let respuesta = await axios.get("https://rickandmortyapi.com/api/character");
+    let personajes = respuesta.data.results;
+    console.log(personajes);
+});
